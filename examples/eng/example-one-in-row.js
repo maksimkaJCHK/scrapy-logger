07b66b2b73ef4de624cafe0ccd3d15d31373ad7a
maksimkaJCHK <maksimkajchk@rambler.ru>
2025-02-19 22:31:51 +0300

    Подготовил пример для русской и английской версии

---
+++ examples/eng/example-one-in-row.js
@@ -0,0 +1,24 @@
+import logger from '../lib/ts-logger.js';
+
+const delayF = (delay = 400) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, delay);
+  });
+};
+
+const oneInRow = async () => {
+  let count = 0;
+
+  logger.disableDate();
+
+  while (count <= 10) {
+    logger.oneMes(`The counter value is ${count}.`);
+
+    count++;
+    await delayF(1_000);
+  }
+
+  logger.oneSuccess('Счетчик закончился');
+}
+
+oneInRow();
